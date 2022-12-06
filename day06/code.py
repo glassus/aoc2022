078a50dd1fd4b760e57b32c917311551f579603b
Gilles Lassus <g.lassus@gmail.com>
2022-12-06 07:02:30 +0100

    maj

---
+++ day06/code.py
@@ -0,0 +1,11 @@
+data = open('input.txt').read().splitlines()[0]
+#data = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+# data = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+#data = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
+
+i = 0
+while len(set(data[i:i+14])) != 14:
+    i += 1
+print(i+14)
+
+print([i+14 for i in range(len(data)) if len(set(data[i:i+14])) == 14][0])
