b1f2ea9fa3d184934b92a086ca233e9c77d3a745
Gilles Lassus <g.lassus@gmail.com>
2022-12-09 17:36:32 +0100

    maj

--- day09/code.py
+++ day09/code.py
@@ -1,80 +1,60 @@
 data = open('input.txt').read().splitlines()
-data = open('input_test.txt').read().splitlines()
+#data = open('input_test.txt').read().splitlines()
 
-import numpy as np
+
 
 instructions = []
 for line in data:
     direction, value = line.split()[0], int(line.split()[1])
     instructions.append([direction, value])
     
-xmax, ymax, xmin, ymin = 0,0,0,0
-x, y = 0,0
-for inst in instructions:
-    if inst[0] == 'R':
-        x += inst[1]
-        xmax = max(x, xmax)
-    if inst[0] == 'L':
-        x -= inst[1]
-        xmin = min(x, xmin)
-    if inst[0] == 'U':
-        y += inst[1]
-        ymax = max(y, ymax)
-    if inst[0] == 'D':
-        y -= inst[1]
-        ymin = min(y, xmin)
-        
-range_x = xmax-xmin + 2
-range_y = ymax-ymin + 2
-start_x = abs(xmin)
-start_y = abs(ymin)
 
-M = np.zeros((range_x, range_y))
-x = start_x
-y = start_y
-xT, yT = x, y
 
-def dist():
-    return abs(xT-x) + abs(yT-y)
+def add(t1,t2):
+    return (t1[0]+t2[0], t1[1]+t2[1])
 
-def follow():
-    global xT, yT
-    delta_x = x - xT
-    delta_y = y - yT
-    if dist() == 2:
+
+d = {'R': lambda t: add(t,(1,0)),
+     'L': lambda t: add(t,(-1,0)),
+     'U': lambda t: add(t,(0,1)),
+     'D': lambda t: add(t,(0,-1))}
+
+start_x = 0
+start_y = 0
+
+visited = set()
+
+size = 10
+snake = [[start_x, start_y] for _ in range(size)]
+
+
+def dist(t1, t2):
+    return abs(t1[0]-t2[0]) + abs(t1[1]-t2[1])
+
+def follow(tH, tT):
+    delta_x = tH[0] - tT[0]
+    delta_y = tH[1] - tT[1]
+    if dist(tH, tT) == 2:
         if delta_x == 2:
-            xT += 1
+            tT = d['R'](tT)
         if delta_x == -2:
-            xT -= 1
+            tT = d['L'](tT)
         if delta_y == 2:
-            yT += 1
+            tT = d['U'](tT)
         if delta_y == -2:
-            yT -= 1
-    if dist() == 3:
-            xT += delta_x // abs(delta_x)
-            yT += delta_y // abs(delta_y)
-    #print("(x,y)", x,y, "(xT,yT)", xT, yT)
-    M[xT,yT] = 1
-        
+            tT = d['D'](tT)
+    if dist(tH,tT) >= 3:
+            tT = add(tT,(delta_x // abs(delta_x), 0))
+            tT = add(tT,(0, delta_y // abs(delta_y)))
 
-
-
+    return tT
+ 
+ 
 for inst in instructions:
-    if inst[0] == 'R':
-        for k in range(inst[1]):
-            x += 1
-            follow()
-    if inst[0] == 'L':
-        for k in range(inst[1]):
-            x -= 1
-            follow()
-    if inst[0] == 'U':
-        for k in range(inst[1]):
-            y += 1
-            follow()
-    if inst[0] == 'D':
-        for k in range(inst[1]):
-            y -= 1
-            follow()
+    for k in range(inst[1]):
+        snake[0] = d[inst[0]](snake[0])
+        for i in range(0, size-1):
+            snake[i+1] = follow(snake[i], snake[i+1])
+        visited.add(tuple(snake[-1]))
 
-print(np.sum(M))
+print(len(visited))
